00bc7868a809e1c01dbdb1d1071d8d43e5ac8aff
オクダユウジ <effortdstrictg@gmail.com>
2026-08-11 20:57:23 +0900

    Create 11.py

---
+++ til/2026/08/11.py
@@ -0,0 +1,8 @@
+# 文字列の中で変数の中身を表示させる方法
+# (文字列の直前にfをつけ、変数を{}で囲う)
+
+last_name = "山田"
+first_name = "太郎"
+sister_name = "花子"
+
+print(f"私の名前は{last_name}{first_name}です。妹の名前は{last_name}{sister_name}です。")
