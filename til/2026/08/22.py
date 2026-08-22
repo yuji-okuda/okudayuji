3b49e866a6ee45374bb80056fffffbaf204fd447
オクダユウジ <effortdstrictg@gmail.com>
2026-08-22 19:40:29 +0900

    Create 22.py

---
+++ til/2026/08/22.py
@@ -0,0 +1,16 @@
+# Python学習記録
+
+## 1. PyCharmのインストール・環境構築
+- JetBrains公式サイトから PyCharm  をインストール
+- プロジェクト作成 、 hello.py を追加
+
+## 2. ターミナルからの出力
+- 内蔵ターミナルを使用
+- `python3` を実行することで、Python使用可能
+- "Hello World" を実行
+- Hello World が出力されることを確認
+
+## 3. Pythonコンソールからの出力
+- Python Console を起動
+- "Hello World" を実行
+- Hello World が出力されることを確認
