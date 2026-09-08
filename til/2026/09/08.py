e54bdc68274271aacc5d3fcd48bb62f530733700
オクダユウジ <effortdstrictg@gmail.com>
2026-09-08 23:41:02 +0900

    Create 08.py

---
+++ til/2026/09/08.py
@@ -0,0 +1,14 @@
+# for文の基礎
+
+nums = [1, 2, 3]              # リストの要素を順番に取り出す
+for n in nums:
+    print(n)
+
+for i in range(5):            # 0〜4を順番に取り出す
+    print(i)
+
+for i in range(2, 6):         # 2〜5を取り出す
+    print(i)
+
+for i in range(0, 10, 2):     # 0〜8を2刻みで取り出す
+    print(i)
