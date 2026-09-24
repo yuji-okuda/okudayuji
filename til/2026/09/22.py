b0ae50265c449b0a5ee90d5844e87b2bdaddfb62
オクダユウジ <effortdstrictg@gmail.com>
2026-09-24 21:20:59 +0900

    Create 22.py

---
+++ til/2026/09/22.py
@@ -0,0 +1,12 @@
+# input関数（str型）
+
+food = input("あなたの好きな食べ物を教えてください。")
+print(food, "を食べに行こう！")
+
+# input関数（int型）
+
+age = input("あなたの年齢を教えてください。")
+age = int(age)　　　※数値を受け取りたい場合、int関数と併用する
+print(f"来年は{age + 1}歳ですね！")
+
+
